808eb6388728357a902e9e7607d43a1eeb3b64f9
VitaliiaStavytska <152968219+VitaliiaStavytska@users.noreply.github.com>
2024-01-16 18:01:34 +0100

    Add files via upload

---
+++ S5.L1.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print( "1.Abbiamo 25 studenti; memorizzare questo dato in una variabile.")
+
+
+# In[5]:
+
+
+studenti=25
+
+
+# In[6]:
+
+
+studenti
+
+
+# In[7]:
+
+
+print("2.Arrivano altri 3 studenti; memorizzare questo dato in un'altra variabile. Creare un'altra variabile ancora che conterrà la somma delle prime due, poi stamparla a video.")
+
+
+# In[14]:
+
+
+altri_studenti =3
+
+
+# In[15]:
+
+
+print (altri_studenti)
+
+
+# In[16]:
+
+
+print("3.Creare una variabile che contiene la stringa Epicode, quindi stamparla a video.")
+
+
+# In[17]:
+
+
+tutti_studenti = studenti + altri_studenti
+
+
+# In[18]:
+
+
+print(tutti_studenti)
+
+
+# In[19]:
+
+
+nome_scuola="Epicode"
+
+
+# In[21]:
+
+
+print(nome_scuola)
+
+
+# In[22]:
+
+
+print("4.Creare, a mano, una lista che contenga i numeri da 0 a 5, memorizzarla in una variabile e infine stamparla a video.")
+
+
+# In[23]:
+
+
+lista=[0,1,2,3,4,5]
+
+
+# In[24]:
+
+
+print(lista)
+
+
+# In[25]:
+
+
+print("5.Stampare le prime tre lettere di parola Epicode")
+
+
+# In[27]:
+
+
+print(nome_scuola[0:3])
+
+
+# In[29]:
+
+
+print("6.Abbiamo la variabile: x = 10 Incrementarla di 2 e poi moltiplicarla per 3 Usare due metodi diversi")
+
+
+# In[30]:
+
+
+x=10
+
+
+# In[31]:
+
+
+print((x+2)*3)
+
+
+# In[33]:
+
+
+y=x+2*3
+
+
+# In[35]:
+
+
+print(y)
+
+
+# In[36]:
+
+
+print("7.Scriviamo un programma che chiede in input all'utente una stringa e visualizza i primi 3 caratteri, seguiti da 3 punti di sospensione e quindi gli ultimi 3 caratteri.")
+
+
+# In[37]:
+
+
+stringa_input = input("la frase")
+
+
+# In[38]:
+
+
+stringa_input = input("la frase")
+
+
+# In[42]:
+
+
+stringa_output=stringa_input[0:4]+"..."+stringa_input[-4:]
+
+
+# In[43]:
+
+
+stringa_output
+
+
+# In[45]:
+
+
+print("8.Verificare, per ognuna delle seguenti stringhe, se il numero di caratteri è compreso tra 5 e 8.")
+
+
+# In[46]:
+
+
+nome1="Windows"
+len(nome1)
+
+
+# In[50]:
+
+
+nome2="Excel"
+len(nome2)
+
+
+# In[51]:
+
+
+nome3="Powerpoint"
+len(nome3)
+
+
+# In[53]:
+
+
+nome4="Word"
+len(nome4)
+
+
+# In[57]:
+
+
+print("il numero dei caratteri di windows ed Excel corrispondono all'intervallo indicato.")
+
+
+# In[59]:
+
+
+print("9.Inventare un programma che prende in input degli elementi e restituisce in output una loro combinazione (ad esempio prendendo in input due numeri e restituendo la loro somma).")
+
+
+# In[61]:
+
+
+stringa=[0,1,2,3,4,5]
+
+
+# In[62]:
+
+
+stringa
+
+
+# In[63]:
+
+
+conti= stringa[2]+stringa[3]
+
+
+# In[65]:
+
+
+print(conti)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
